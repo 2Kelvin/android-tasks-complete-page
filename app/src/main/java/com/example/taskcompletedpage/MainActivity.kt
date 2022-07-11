ae63d43d7be83f40e77a645fbc98c7c44f65cc92
2Kelvin <kvin.mwangi254@gmail.com>
2022-07-11 22:57:48 +0300

    text-aligned composables to center-horizontally

--- app/src/main/java/com/example/taskcompletedpage/MainActivity.kt
+++ app/src/main/java/com/example/taskcompletedpage/MainActivity.kt
@@ -4,17 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,7 +45,8 @@ fun ImageCheck() {
     val image = painterResource(R.drawable.ic_task_completed)
     Image(
         painter = image,
-        contentDescription = "tasks complete"
+        contentDescription = "tasks complete",
+        modifier = Modifier.fillMaxWidth()
     )
 }
 
@@ -54,8 +55,11 @@ fun TasksComplete(textBold:String) {
     Text(
         text = textBold,
         fontSize = 24.sp,
-        modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
-        fontWeight = FontWeight.Bold
+        modifier = Modifier
+            .padding(top = 24.dp, bottom = 8.dp)
+            .fillMaxWidth(),
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
     )
 }
 
@@ -63,13 +67,15 @@ fun TasksComplete(textBold:String) {
 fun TextNice(niceText:String) {
     Text(
         text = niceText,
-        fontSize = 16.sp
+        fontSize = 16.sp,
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center
     )
 }
 
 @Composable
 fun PageComplete(textBold: String, niceText: String) {
-    Column {
+    Column(Modifier.fillMaxHeight()) {
         ImageCheck()
         TasksComplete(textBold)
         TextNice(niceText)
